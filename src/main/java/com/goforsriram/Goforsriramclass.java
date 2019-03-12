7b08be809728f6022f1fd02cb4456ad641f5ea05
sriram <sriramdvhere@gmail.com>
2019-03-12 22:24:31 +0530

    updating java source and POM
    updating java source and POM

---
+++ src/main/java/com/goforsriram/Goforsriramclass.java
@@ -0,0 +1,13 @@
+package com.goforsriram;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Goforsriramclass
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("Hello to Java and maven and git guys...");
+	}
+}
