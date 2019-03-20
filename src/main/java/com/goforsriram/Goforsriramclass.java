2bd4871daa5986f3448ea98f9fb82f59ec99e7a0
sriram <sriramdvhere@gmail.com>
2019-03-20 23:48:32 +0530

    Adding test classes

--- src/main/java/com/goforsriram/Goforsriramclass.java
+++ src/main/java/com/goforsriram/Goforsriramclass.java
@@ -6,8 +6,20 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Goforsriramclass
 {
-	public static void main (String[] args) throws java.lang.Exception
+	static String var = "initial";
+	/*public static void main (String[] args) throws java.lang.Exception
 	{
-		System.out.println("Hello to Java and maven and git guys...");
+		var = printKeyWord();
+		System.out.println("Hello to Java and maven and git guys..."+" keyword is "+ var);
 	}
+	*/
+	
+	public static String printKeyWord(){
+		return Goforsriramclass.var;
+	}
+	
+	public static int getLucky() {
+        return 7;
+    }
+
 }
